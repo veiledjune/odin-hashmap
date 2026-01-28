843119ed08b2cd7452bf72ec083736e291281c86
Cameron Brown <veiledjune@gmail.com>
2026-01-28 03:44:21 +0200

    feat: Add HashMap set method and add a variable to track total array items

--- hashmap.js
+++ hashmap.js
@@ -1,7 +1,10 @@
+import { LinkedList, Node } from './linked-list.js';
+
 export class HashMap {
   constructor() {
     this.capacity = 16;
     this.array = new Array(this.capacity);
+    this.totalItems = 0;
     this.loadFactor = Math.round(0.75 * this.capacity);
   }
 
@@ -12,5 +15,29 @@ export class HashMap {
       hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
     }
     return hashCode;
+  }
+
+  set(key, value) {
+    const arr = this.array;
+    const hashCode = this.hash(key);
+    const node = new Node(key, value);
+    if (!arr[hashCode]) {
+      arr[hashCode] = new LinkedList();
+      arr[hashCode].head = node;
+      this.totalItems++;
+    } else {
+      let current = arr[hashCode].head;
+      while (current) {
+        if (current.key === key) {
+          current.value = value;
+          return;
+        }
+        if (!current.nextNode) {
+          current.nextNode = node;
+          this.totalItems++;
+          return;
+        } else current = current.nextNode;
+      }
+    }
   }
 }
